e1641dd6ec7f5c1407bdca873c8d3df4afe1e7ff
Thales <thales.souz@outlook.com>
2024-02-13 15:06:00 -0300

    feat: config app module

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,35 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { INestApplication, Logger } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { ConfigService } from '@nestjs/config';
+import compression from 'compression';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app: INestApplication = (
+    await NestFactory.create<NestExpressApplication>(AppModule)
+  ).setGlobalPrefix('api/v1');
+  const logger = new Logger(AppModule.name);
+  const configService = app.get<ConfigService>(ConfigService);
+
+  app.enableCors({
+    origin: true,
+    credentials: true,
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
+    allowedHeaders:
+      'Content-Type,Accept,Authorization,Access-Control-Allow-Origin',
+  });
+
+  app.use(compression());
+
+  await app.startAllMicroservices();
+
+  await app.listen(configService.get('PORT'));
+
+  logger.log(
+    `${AppModule.name} server is running on PORT: ${await configService.get(
+      'PORT',
+    )}`,
+  );
 }
 bootstrap();
