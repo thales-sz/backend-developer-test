d037bd2e773c1bcd8f33af11b7b606bdb7736342
Thales <thales.souz@outlook.com>
2024-02-13 17:49:09 -0300

    fix: change Job entity to match intial SQL file

--- api/src/domain/entity/company.entity.ts
+++ api/src/domain/entity/company.entity.ts
@@ -1,11 +1,15 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 import { AbstractEntity } from './abstract.entity';
+import { Job } from './job.entity';
 
 @Entity('companies')
 export class Company extends AbstractEntity<Company> {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
-  @Column()
+  @Column({ type: 'text', unique: true })
   name: string;
+
+  @OneToMany(() => Job, (job) => job.company)
+  jobs: Job[];
 }
--- api/src/domain/entity/job.entity.ts
+++ api/src/domain/entity/job.entity.ts
@@ -1,24 +1,35 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 import { Company } from './company.entity';
 import { AbstractEntity } from './abstract.entity';
+import { JobStatus } from '../enum/job-status.enum';
 
 @Entity('jobs')
 export class Job extends AbstractEntity<Job> {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
-  @Column()
-  title: string;
-
-  @Column()
-  description: string;
-
-  @Column()
+  @ManyToOne(() => Company, (company) => company.jobs)
+  @JoinColumn({ name: 'company_id' })
   company: Company;
 
-  @Column()
+  @Column({ type: 'text', nullable: false })
+  title: string;
+
+  @Column({ type: 'text', nullable: false })
+  description: string;
+
+  @Column({ type: 'text', nullable: false })
   location: string;
 
-  @Column()
-  status: string;
+  @Column({ type: 'text', nullable: true })
+  notes: string;
+
+  @Column({ type: 'enum', enum: JobStatus, default: JobStatus.DRAFT })
+  status: JobStatus;
 }
